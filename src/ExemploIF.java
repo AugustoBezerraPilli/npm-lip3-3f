6b3f9316af0aee1f5c21935fe1c7bb6db773021c
Augusto da Silva Bezerra <rm79745@estudante.fieb.edu.br>
2022-03-18 12:16:32 -0300

    Primeiro Commit

---
+++ src/ExemploIF.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class ExemploIF {
+
+	public static void main(String[] args) {
+		
+	double media = Double.parseDouble(JOptionPane.showInputDialog(null, "Média!"));
+	
+	if ( media >= 6.0 ) {
+		JOptionPane.showMessageDialog(null, "aprovado");
+	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, );"reprovado!");
+	}
+		
+	
+	
+	
+	}
+};
---
+++ src/ResultadoAluno.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class ResultadoAluno {
+
+	public static void main(String[] args) {
+		double media = ?; 
+		int x = 10;
+		int y = 5;
+		
+		}	if( x > y ) {
+			JOptionPane.showMessageDialog(null,
+					x + " " + y);
+		} else if ( x < y ) {
+			JOptionPane.showMessageDialog(null,
+					x +" " + y);
+		} else {
+			JOptionPane.showMessageDialog(null,
+					x + "  " + y);
+		}
+
+	}
+
+}
